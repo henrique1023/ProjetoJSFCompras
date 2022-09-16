769f948da761cc05d6dd1376f18a44cdb87fd6a5
henrique1023 <henrique.amorim@fatec.sp.gov.br>
2022-09-16 16:09:03 -0300

    14# criado o botão para editar, e excluir e função de editar

--- src/main/java/br/com/dio/bean/EmployeeBean.java
+++ src/main/java/br/com/dio/bean/EmployeeBean.java
@@ -2,28 +2,41 @@ package br.com.dio.bean;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.faces.context.Flash;
 import javax.faces.view.ViewScoped;
-import javax.inject.Inject;
 import javax.inject.Named;
+
+import org.primefaces.PrimeFaces;
+import org.primefaces.event.SelectEvent;
 
 import br.com.dio.model.Employee;
 import br.com.dio.service.EmployeeService;
 
 @Named
 @ViewScoped
-public class EmployeeBean implements Serializable{
+public class EmployeeBean implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-	
+
 	private EmployeeService service = new EmployeeService();
-	
+
 	private Employee employee = new Employee();
-	
-	private List<String> Funcoes;
-	
+
 	private List<Employee> employees = new ArrayList<>();
+
+	public void init() {
+		Flash flash = FacesContext.getCurrentInstance().getExternalContext().getFlash();
+		employee = (Employee) flash.get("employee");
+		if (employee == null) {
+			employee = new Employee();
+		}
+	}
 
 	public Employee getEmployee() {
 		return employee;
@@ -40,32 +53,40 @@ public class EmployeeBean implements Serializable{
 	public void setEmployees(List<Employee> employees) {
 		this.employees = employees;
 	}
-	
-	public String addEmployee(){
-		employee.setId(-1);
+
+	public String addEmployee() {
+		if (employee.getId() == null) {
+			employee.setId(-1);
+		}
 		service.saveOrUptade(employee);
 		clean();
 		return "form_employee.xhtml?faces-redirect=true";
 	}
-	
+
 	public void searchAllEmployee() {
 		employees = service.findAll();
 	}
-	
+
 	public void clearListEmployee() {
 		employees = null;
 	}
-	
+
 	private void clean() {
 		this.employee = new Employee();
 	}
-	
-	public List<String> getFuncoes(){
+
+	public List<String> getFuncoes() {
 		List<String> funcoes = new ArrayList<String>();
 		funcoes.add("Administrador");
 		funcoes.add("Vendedor");
 		funcoes.add("Analista");
 		return funcoes;
 	}
-	
+
+	public String editar(Employee e) {
+		Flash flash = FacesContext.getCurrentInstance().getExternalContext().getFlash();
+		flash.put("employee", e);
+		return "register_employee?faces-redirect=true";
+	}
+
 }
